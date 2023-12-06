0db1bcd6ff93228507f7045cc7746382d2fc8783
user <user@users-MBP-6.cust.communityfibre.co.uk>
2023-12-06 20:18:17 +0000

    new lessons

---
+++ app.js
@@ -0,0 +1,69 @@
+// strings
+console.log("hello world"[10])
+console.log('223'.length)
+let str ='david'
+console.log(str[0])
+console.log(str[str .length -1])
+
+
+// number data types
+// check  the html
+
+
+// boolean(true or false)
+
+
+// null and undefined
+// check html
+
+//variables
+
+let fullName  = 'sade ashkir'
+let tempreture = 20
+console.log(tempreture + 2 )
+
+
+let celcius = 10;
+let farenheit = celcius * 1.8 + 32;
+console.log(farenheit)
+
+
+let bool = 1 == "1"
+console.log(bool)
+// true 
+let boole = 1 === "1"
+console.log(boole)
+// false
+// always try to use ===
+
+
+// if else
+
+let subscribed = false
+let loggedIn = true
+
+if (subscribed === true) {
+  console.log("show the video")
+}
+else if (loggedIn === true) {
+  console.log('tell the user to upgrade their subscription')
+}
+
+else{
+console.log('tell user to log into account')
+}
+
+let cash = 100
+let price = 40
+let diffrence = cash - price
+if (cash > price) {
+  console.log(`you paid extra - here's ${diffrence} dollars change`)
+}
+else if (cash === price) {
+console.log("you paid the exact amount, have a nice day")
+}
+else {
+  console.log(`not enough you still owe ${diffrence * -1} amount`)
+}
+
+
