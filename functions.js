7b20444a57016e8e7b6afa7f42b05a207e0f6e9e
user <user@users-MBP-6.cust.communityfibre.co.uk>
2023-12-08 23:29:58 +0000

    addition to js coursework(functions)

---
+++ functions.js
@@ -0,0 +1,9 @@
+
+// function definition
+function welcomePersonToFES(name){
+   console.log(`welcome to fes,${name}`)
+
+}
+// call the funtion
+welcomePersonToFES("sade");  
+//1;24;00
--- loops.js
+++ loops.js
@@ -1,6 +1,69 @@
-let count = 1
 
-for( let i = 0; i < 3; i = i++) {
-console.log(i)
-}
-// public
+
+//  for( let i = 0; i < 3; i = i++) {
+// console.log(i)
+// }
+
+ 
+//  for (let i=0; i <= 20; ++i) {
+//   if( i%3 ===0){
+//     console.log(`${i} ->frontend`)
+//   }
+//   else if( i%5 ===0){
+//     console.log(`${i} ->simplified`)
+//   }
+//   else if( i%3 ===0 && i%5 ===0  ){
+//     console.log(`${i} ->frontend simplifed`)
+//   }
+ 
+//   else {
+//     console.log(`${i} -> ${i}`)
+ 
+ 
+  
+// }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for(let i=0; i <= 20; ++i ){
+  if( i%3 ===0){
+    console.log(`${i} ->frontend`)
+
+  }
+  elseif(i%5 ===0);{
+    console.log(`${i} -> simplified`)
+  }
+  elseif(i%3 ===0 && i%5 ===0);{
+    console.log(`${i} -> frontend simplified`)
+   }
+
+ {
+    console.log(`${i} ->${i}`)
+   }
+ }
+
+
+
+
+
+
